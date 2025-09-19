a9db28f26d33a6130fc078af5d7581f7c7d41eef
Trachelle <Trachellehaddox2007@gmail.com>
2025-09-19 15:11:29 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+game_name = "Piggy from Hell"
+print("Welcome to Piggy from Hell!")
+print("===========================")
+
+# Ask for the character's name 
+name = input()
+
+# Print the name
+print("Rowan Thorn")
