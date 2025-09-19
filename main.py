9414840753819b34230cd359a2196bbcb10579cb
Trachelle <Trachellehaddox2007@gmail.com>
2025-09-19 15:34:43 -0400

    Fixed problems

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 game_name = "Piggy from Hell"
-print("Welcome to Piggy from Hell!")
+print("Welcome to " + game_name  + "!")
 print("===========================")
 
 # Ask for the character's name 
-name = input()
+name = input("Before we begin, what is your charcter's name? \n> ")
 
 # Print the name
-print("Rowan Thorn")
+print("Great, " +  name + "! " + "Let's begin the adventure!")
