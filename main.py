d9535d297d74429326b6364b2406aadb62fcbedd
Trachelle <Trachellehaddox2007@gmail.com>
2025-10-02 18:58:59 -0400

    Added character stats

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 game_name = "Piggy from Hell"
-print("Welcome to " + game_name  + "!")
+print(f"Welcome to {game_name}!")
 print("===========================")
 
 # Ask for the character's name 
 name = input("Before we begin, what is your charcter's name? \n> ")
 
 # Print the name
-print("Great, " +  name + "! " + "Let's begin the adventure!")
+print(f"Great, {name}! Let's begin the adventure!")
+
+# Build character stats dictionary
+player = {
+    "name": name,
+    "health": 100,
+    "coin": 0
+}
+
+# Print player stats
+print(f"Character stats: {player}") 
+
+import random
+
+events = ["find a coin", "meet a monster", "do nothing"]
+event = random.choice(events)
+print(f"While exploring, you {event}!")
+
+# Update stats
+if event == "find a coin":
+    player["coin"] += 1
+    print(f"{player['name']} found a coin! Now has {player['coin']} coins.")
+elif event == "meet a monster":
+    player["health"] -= 10
+    print(f"oh no! {player['name']} fought a monster and now has {player['health']} health.")
+else:
+    print(f"{player['name']} wandered around but nothing happened.")
+
+# Print updated player stats 
+print(f"Updated character stats: {player}")
