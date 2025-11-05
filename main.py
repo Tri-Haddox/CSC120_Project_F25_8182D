5b34de907e8a6a97e8a18a951c9314bbaaa416de
Your Name <trachellehaddox2007@gmail.com>
2025-11-05 18:40:29 -0500

    added map

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 game_name = "Zombie Break"
 print(f"Welcome to {game_name}!")
 print("===========================")
@@ -20,9 +22,9 @@ player = {
 # Print player stats
 print(f"Character stats: {player}") 
 
-import random
-
 events = ["find a coin", "meet a monster", "do nothing"]
+
+map_size = 9
 
 def check_event():
     global events, player
@@ -34,4 +36,56 @@ elif events == "meet a monster":
     player["health"] -= 10
 else:
     pass
+
+def draw_ui(x, y):
+    print("=" * 25)
+    for i in range(map_size):
+        for j in range(map_size):
+            if i == y and j == x:
+                print("C", end="  ")
+            elif i == map_size - 1 and j == map_size - 1:
+                print("M", end="  ")
+            else:
+                print(".", end="  ")
+        print()
+        
+    print("=" * 25)
+    print(f"Health: {player['health']}")
+    print("-" * 25)
+    print(f"Coin: {player['coin']}")
+    print("=" * 25)
+        
+def move(direction):
+    global player
+
+    if direction == 'w' and player['y'] > 0:
+        player['y'] -= 1
+    elif direction == 'a' and player['x'] > 0:
+        player['x'] -= 1
+    elif direction == 's' and player['y'] < map_size - 1:
+        player['y'] += 1
+    elif direction == 'd' and player['x'] < map_size - 1:
+        player['x'] += 1
+    else:
+        print("You cannot move that way!")
+
+def main():
+    draw_ui(player['x'], player['y'])
+    direction = input("Your next move (w/a/s/d/q): ")
+
+    while direction != 'q':
+        move(direction)
+
+        if player['x'] == map_size - 1 and player['y'] == map_size - 1:
+            print("Congratulations! You reach the gate for next level.")
+            break
+
+        check_event()
+
+        draw_ui(player['x'], player['y'])
+
+        direction = input("Your next move (w/a/s/d/q): ")
+    
+if __name__ == '__main__':
+     main()
 
