6a528040e438bfcb0ddbfa32b238fff978eb3050
Your Name <trachellehaddox2007@gmail.com>
2025-11-05 19:19:32 -0500

    added map

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ player = {
     "x": 0,
     "y": 0
 }
-
 # Print player stats
 print(f"Character stats: {player}") 
 
@@ -41,14 +40,14 @@ def draw_ui(x, y):
     print("=" * 25)
     for i in range(map_size):
         for j in range(map_size):
-            if i == y and j == x:
+            if i == x and j == y:
                 print("C", end="  ")
             elif i == map_size - 1 and j == map_size - 1:
                 print("M", end="  ")
             else:
                 print(".", end="  ")
+
         print()
-        
     print("=" * 25)
     print(f"Health: {player['health']}")
     print("-" * 25)
@@ -58,14 +57,14 @@ def draw_ui(x, y):
 def move(direction):
     global player
 
-    if direction == 'w' and player['y'] > 0:
-        player['y'] -= 1
-    elif direction == 'a' and player['x'] > 0:
+    if direction == 'w' and player['x'] > 0:
         player['x'] -= 1
-    elif direction == 's' and player['y'] < map_size - 1:
-        player['y'] += 1
-    elif direction == 'd' and player['x'] < map_size - 1:
+    elif direction == 'a' and player['y'] > 0:
+        player['y'] -= 1
+    elif direction == 's' and player['x'] < map_size - 1:
         player['x'] += 1
+    elif direction == 'd' and player['y'] < map_size - 1:
+        player['y'] += 1
     else:
         print("You cannot move that way!")
 
@@ -81,9 +80,7 @@ def main():
             break
 
         check_event()
-
         draw_ui(player['x'], player['y'])
-
         direction = input("Your next move (w/a/s/d/q): ")
     
 if __name__ == '__main__':
